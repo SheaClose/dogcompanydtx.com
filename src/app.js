614af55e674c3941aedf49f3bf36483664080467
SheaClose <SheaClose@gmail.com>
2016-10-04 19:52:20 -0500

    Main Page views created,

---
+++ src/app.js
@@ -0,0 +1,25 @@
+angular.module("app", ["ui.router"])
+.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise("/");
+  $stateProvider
+  .state("home", {
+    url: "/"
+    , templateUrl: "../src/components/home/homeTmpl.html"
+  })
+  .state("store", {
+    url: "/store"
+    , templateUrl: "../src/components/store/storeTmpl.html"
+  })
+  .state("shows", {
+    url: "/shows"
+    , templateUrl: "../src/components/shows/showsTmpl.html"
+  })
+  .state("about", {
+    url: "/about"
+    , templateUrl: "../src/components/about/aboutTmpl.html"
+  })
+  .state("media", {
+    url: "/media"
+    , templateUrl: "../src/components/media/mediaTmpl.html"
+  })
+})
