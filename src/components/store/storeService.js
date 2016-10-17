b498b13faceee6be2778cc841b6dc7c9466fb7c5
SheaClose <SheaClose@gmail.com>
2016-10-17 14:34:52 -0500

    store was missing, added back

--- src/components/store/storeService.js
+++ src/components/store/storeService.js
@@ -1,9 +1,7 @@
 angular.module("app")
 .service("storeService", function($http){
   this.getProducts = () =>{
-    console.log("StoreService / getProducts");
     return $http.get("/api/products").then((response)=>{
-      console.log("storeService / getProducts.then");
       return response;
     })
   }
