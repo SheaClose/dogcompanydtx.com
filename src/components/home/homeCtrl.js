547c56ab59444bf851975253d9515b62c1fb1bb9
SheaClose <SheaClose@gmail.com>
2016-10-17 12:19:30 -0500

    updated mong db

--- src/components/home/homeCtrl.js
+++ src/components/home/homeCtrl.js
@@ -2,7 +2,6 @@ angular.module("app")
 .controller("homeCtrl", function($scope, homeService){
   function getBlogs() {
     homeService.getBlogs().then(function(response){
-      console.log(response);
       $scope.blogs = response
       })
   }
