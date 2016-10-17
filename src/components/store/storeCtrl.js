88f12aa027c4220714359f9d2127f6f1ff29d4de
SheaClose <SheaClose@gmail.com>
2016-10-17 15:21:22 -0500

    correcting text size in mobile view

--- src/components/store/storeCtrl.js
+++ src/components/store/storeCtrl.js
@@ -1,9 +1,7 @@
 angular.module("app")
 .controller("storeCtrl", function($scope, storeService){
-  $scope.show = false;
   const getProducts = () => {
     storeService.getProducts().then((response) => {
-      $scope.show = true;
       $scope.products = response.data
     })
   }
