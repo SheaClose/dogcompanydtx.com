709bdf50ccfa046060141b81d8ce7233db220c6f
SheaClose <SheaClose@gmail.com>
2016-10-18 12:22:32 -0500

    minor design changes

--- src/components/store/productCtrl.js
+++ src/components/store/productCtrl.js
@@ -3,10 +3,24 @@ angular.module("app")
   const getAllProducts = () => {
     storeService.getAllProducts().then((response) => {
       let product = response.data;
-      $scope.products = [];
+      // $scope.products = [];
       product.forEach((cv, i, a)=>{
         if (cv._id === $state.params.id) {
-          $scope.products.push(cv)
+          // console.log(cv);
+          $scope.product = {
+            _id: cv._id
+            , category: cv.category
+            , color: cv.color
+            , description: cv.description
+            , imgUrl: cv.imgUrl
+            , price: cv.price
+            , title: cv.title
+          }
+          if (cv.category !== "merch"){
+            $scope.product.size = cv.size;
+          }
+          console.log($scope.product);
+          // $scope.products.push(cv)
         }
       })
     })
