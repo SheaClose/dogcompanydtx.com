f4c981f6a14415402cd887d5af57b799becb5776
SheaClose <SheaClose@gmail.com>
2017-12-16 14:26:38 -0600

    fixed non-us address issue

--- server/features/Order/orderCtrl.js
+++ server/features/Order/orderCtrl.js
@@ -1,71 +1,69 @@
 "use strict";
 
 const Order = require("./Order.js"),
-	serverConfig = require("../../../serverConfig.js"),
-	sg = require("sendgrid")(serverConfig.SENDGRID_API_KEY);
+  serverConfig = require("../../../serverConfig.js"),
+  sg = require("sendgrid")(serverConfig.SENDGRID_API_KEY);
 
 module.exports = {
-	submitOrder: (req, res) => {
-		new Order(req.body).save((err, order) => {
-			if (err) {
-				return res.status(500).json(err);
-			} else {
-				let address = ``;
-				if (req.body.user.nonUSAddress) {
-					address = req.body.user.nonUSAddress;
-				} else {
-					address = `${req.body.user.street}, ${req.body.user.city}, ${req.body
-						.user.state} ${req.body.user.zipcode}`;
-				}
-				const orderProductArray = [];
-				req.body.cart.forEach((c, i, a) => {
-					let tempProduct;
-					if (c.product.category === "apparel") {
-						tempProduct = `
-						Product: ${c.product.title},
-						Price: ${c.product.price},
-						Color: ${c.product.color},
-						Size: ${c.product.size},
-						Quantity: ${c.quantity},
-						description: ${c.product.description},
-						Total: ${c.total}
+  submitOrder: (req, res) => {
+    new Order(req.body).save((err, order) => {
+      if (err) {
+        return res.status(500).json(err);
+      } else {
+        let address =
+          req.body.user.nonUSAddress ||
+          `${req.body.user.street}, ${req.body.user.city}, ${
+            req.body.user.state
+          } ${req.body.user.zipcode}`;
+        const orderProductArray = [];
+        req.body.cart.forEach((c, i, a) => {
+          let tempProduct;
+          if (c.product.category === "apparel") {
+            tempProduct = `
+Product: ${c.product.title},
+Price: ${c.product.price},
+Color: ${c.product.color},
+Size: ${c.product.size},
+Quantity: ${c.quantity},
+description: ${c.product.description},
+Total: ${c.total}
 						\n`;
-					} else {
-						tempProduct = `
-						Product: ${c.product.title},
-						Price: ${c.product.price},
-						Quantity: ${c.quantity},
-						description: ${c.product.description},
-						Total: ${c.total}
+          } else {
+            tempProduct = `
+Product: ${c.product.title},
+Price: ${c.product.price},
+Quantity: ${c.quantity},
+description: ${c.product.description},
+Total: ${c.total}
 						\n`;
-					}
+          }
 
-					orderProductArray.push(tempProduct);
-				});
-				let orderProductString = orderProductArray.join(" ");
-				const request = sg.emptyRequest({
-					method: "POST",
-					path: "/v3/mail/send",
-					body: {
-						personalizations: [
-							{
-								to: [
-									{
-										email: req.body.user.email
-									}
-								],
-								subject: `Your Dog Company Merchandise Order(${order._id})`
-							}
-						],
-						from: {
-							email: "DogCompanyDtx@gmail.com"
-						},
-						content: [
-							{
-								type: "text/plain",
-								value: `Hello, ${req.body.user.first_name}!
+          orderProductArray.push(tempProduct);
+        });
+        let orderProductString = orderProductArray.join(" ");
+        const request = sg.emptyRequest({
+          method: "POST",
+          path: "/v3/mail/send",
+          body: {
+            personalizations: [
+              {
+                to: [
+                  {
+                    email: req.body.user.email
+                  }
+                ],
+                subject: `Your Dog Company Merchandise Order(${order._id})`
+              }
+            ],
+            from: {
+              email: "DogCompanyDtx@gmail.com"
+            },
+            content: [
+              {
+                type: "text/plain",
+                value: `Hello, ${req.body.user.first_name}!
 
-								Thank you for shopping for Dog Company Merchandise! We have received your order, please give us a couple of days to fullfil your order. We will send you an e-mail once we have confirmed your order, total, and shipping costs. We accept Check and Paypal. If for any reason you need to change or cancel your order, you can reach us at DogCompanyDtx@gmail.com.
+	Thank you for shopping for Dog Company Merchandise! We have received your order, please give us a couple of days to fullfil your order. We will send you an e-mail once we have confirmed your order, total, and shipping costs. We accept Check and Paypal. If for any reason you need to change or cancel your order, you can reach us at DogCompanyDtx@gmail.com.
 
 Order Details:
 	Order Number: ${order._id}
@@ -75,16 +73,16 @@ Order Details:
 	Total: $${req.body.total}
 	Cart:
 	${orderProductString}`
-							}
-						]
-					}
-				});
-				sg.API(request, function(error, response) {
-					if (error) {
-					}
-				});
-				return res.status(200).json(order);
-			}
-		});
-	}
+              }
+            ]
+          }
+        });
+        sg.API(request, function(error, response) {
+          if (error) {
+          }
+        });
+        return res.status(200).json(order);
+      }
+    });
+  }
 };
--- src/components/cart/cartCtrl.js
+++ src/components/cart/cartCtrl.js
@@ -1,13 +1,12 @@
-app.controller('cartCtrl', [
-  '$scope',
-  '$window',
-  'cartService',
-  'storeService',
-  '$state',
-  function cartCtrl($scope, $window, cartService, storeService, $state) {
+app.controller("cartCtrl", [
+  "$scope",
+  "cartService",
+  "storeService",
+  "$state",
+  function cartCtrl($scope, cartService, storeService, $state) {
     angular.element(document).ready(() => {
       var x;
-      $(window).on('scroll', function() {
+      $(window).on("scroll", function() {
         var x = $(window).scrollTop();
         function retY() {
           var y = $(window).scrollTop() / $(window).height();
@@ -17,27 +16,26 @@ app.controller('cartCtrl', [
             return 0.85;
           }
         }
-        $('.Store-page-container').css(
-          'background-size',
-          125 + parseInt(x / 3) + 'vh'
+        $(".Store-page-container").css(
+          "background-size",
+          125 + parseInt(x / 3) + "vh"
         );
-        $('.Store-page-content-container').css(
-          'background-color',
-          'rgba(0,0,0, ' + retY() + ')'
+        $(".Store-page-content-container").css(
+          "background-color",
+          "rgba(0,0,0, " + retY() + ")"
         );
       });
-      $('.modal-trigger').leanModal();
+      $(".modal-trigger").leanModal();
     });
     $scope.reviewProduct = function(product) {
       let selectedItem = storeService.products.find(
         cur => cur._id == product._id
       );
-      $state.go('product', { id: selectedItem.title });
+      $state.go("product", { id: selectedItem.title });
     };
-    $scope.unitedStatesAddress = true;
     const fillCart = id => {
       cartService.fillCart(id).then(function(response) {
-        $scope.cart = response.data.cart.map((cv) => {
+        $scope.cart = response.data.cart.map(cv => {
           return {
             product: cv.product,
             quantity: cv.quantity,
@@ -46,9 +44,6 @@ app.controller('cartCtrl', [
         });
         getOrderTotal();
       });
-    };
-    $scope.click = () => {
-      $scope.unitedStatesAddress = !$scope.unitedStatesAddress;
     };
     const getCart = () => {
       cartService.getCart().then(function(response) {
@@ -89,21 +84,24 @@ app.controller('cartCtrl', [
         getCart();
       });
     };
-    $scope.submitOrder = () => {
-      let userInfo = {};
-      if ($scope.unitedStatesAddress) {
-        if (
-          $scope.first_name === undefined ||
-          $scope.last_name === undefined ||
-          $scope.email === undefined ||
-          $scope.street === undefined ||
-          $scope.city === undefined ||
-          $scope.state === undefined ||
-          $scope.zipcode === undefined
-        ) {
-          alert(`Please ensure all fields are complete. We can not complete your order without this information.
 
-					If you are having trouble completing an order, Please contact us at DogCompanyDtx@gmail.com`);
+    $scope.submitOrder = () => {
+      let userInfo;
+      if (!$scope.foreignAddress) {
+        if (
+          !$scope.first_name ||
+          !$scope.last_name ||
+          !$scope.email ||
+          !$scope.street ||
+          !$scope.city ||
+          !$scope.state ||
+          !$scope.zipcode
+        ) {
+          alert(
+            `Please ensure all fields are complete. We can not complete your order without this information.
+
+If you are having trouble completing an order, Please contact us at DogCompanyDtx@gmail.com`
+          );
           return;
         }
         userInfo = {
@@ -116,12 +114,20 @@ app.controller('cartCtrl', [
           zipcode: $scope.zipcode
         };
       } else {
-        userInfo = {
-          first_name: $scope.first_name,
-          last_name: $scope.last_name,
-          email: $scope.email,
-          nonUSAddress: $scope.nonUsAddress
-        };
+        if (!$scope.nonUsAddress) {
+          alert(
+            `Please ensure all fields are complete. We can not complete your order without this information.
+If you are having trouble completing an order, Please contact us at DogCompanyDtx@gmail.com`
+          );
+          return;
+        } else {
+          userInfo = {
+            first_name: $scope.first_name,
+            last_name: $scope.last_name,
+            email: $scope.email,
+            nonUSAddress: $scope.nonUsAddress
+          };
+        }
       }
       const order = {
         cart: $scope.cart,
@@ -131,11 +137,11 @@ app.controller('cartCtrl', [
       cartService.submitOrder(order).then(response => {
         window.currentUserOrderInformation = response.data;
         cartService.deleteUser().then(response => {
-          $window.location.hash = '#!/store';
-          // $window.location.href = "http://127.0.0.1:4000/#/store";
+          $state.go("store");
         });
       });
     };
+
     getCart();
   }
 ]);
